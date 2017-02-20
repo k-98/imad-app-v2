62b12183f624b5b8e22dfae8638852b38b91e720
k-98 <tiwari.kishan12@gmail.com>
2017-02-20 19:20:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 
+var button  = document.getElementById('counter');
+button.onclick = function(){
+    // Make A request
+    
+    
+    
+    
+    
+    
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
